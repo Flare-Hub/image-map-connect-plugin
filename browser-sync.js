4c4de496109b5fe19b5293b2fe18fb2607d6425f
igor <igor@flarehub.io>
2022-11-17 17:15:00 +0200

    Fix redirect caching issue

--- browser-sync.js
+++ browser-sync.js
@@ -6,12 +6,19 @@ const wait = require('wait-on');
 
 const server = 'http://localhost:' + process.env.WP_PORT_MAP || 8000
 
+console.log('Waiting on Wordpress at: ', server);
 // Wait for server to be available
 wait({ resources: [server] })
 	// Then proxy the server through browser-sync
 	.then(() => browserSync({
 		// Proxy the docker web server
 		proxy: server,
+		proxyReq: [
+			function (proxyReq, req, res) {
+				proxyReq.setHeader('Host', req.headers.host);
+			}
+		],
+		port: process.env.BROWSERSYNC_PORT,
 		// Refresh on js, css or php changes
 		files: [
 			"inc/**/*.php",
