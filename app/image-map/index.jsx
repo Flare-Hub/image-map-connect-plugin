de7d59c934f57c0a3d01304b35ea7ba3bb0c4e36
igor <igor@flarehub.io>
2023-04-03 16:35:53 +0300

    Add OpenLayer css to backend app

--- app/image-map/index.jsx
+++ app/image-map/index.jsx
@@ -2,7 +2,9 @@ import { render } from '@wordpress/element'
 
 import { RouterProvider } from './contexts/router'
 import App from './app'
+
 import './styles.scss'
+import 'ol/ol.css'
 
 const appDiv = document.getElementById('image-map')
 
