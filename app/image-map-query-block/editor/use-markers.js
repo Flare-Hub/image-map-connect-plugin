baf9ce03eb0bc81d3d3bed644cb3b0da6ba2c9b9
igor <igor@flarehub.io>
2023-07-06 20:34:33 +0300

    Show all markers in editor when not in query loop

--- app/image-map-query-block/editor/use-markers.js
+++ app/image-map-query-block/editor/use-markers.js
@@ -3,18 +3,6 @@ import { __ } from '@wordpress/i18n';
 import useNotice from 'common/utils/use-notice';
 
 /**
- * Get list of loaded records or empty list if the records should not be included.
- *
- * @param {boolean}                                            include Include the records in the marker list.
- * @param {Array<import('@wordpress/core-data').EntityRecord>} list    The records from useEntityRecords.
- */
-function listIfIncluded( include, list ) {
-	if ( ! include ) return [];
-	if ( ! list ) return false;
-	return list;
-}
-
-/**
  * Get marker details from provided posts and standalone markers
  *
  * @param {number}       map            The current map.
@@ -22,57 +10,52 @@ function listIfIncluded( include, list ) {
  * @param {string|false} posts          Comma separated list of posts.
  * @param {boolean}      showStandAlone Whether to show standalone markers.
  */
-export default function useMarkers( map, layer, posts, showStandAlone ) {
+export default function useMarkers(map, layer, posts, showStandAlone) {
 	// Get marker details for provided post IDs.
-	const linkedQuery = posts
-		? {
-				layers: layer,
-				_fields: 'id,imc_icons,imc_loc,type',
-				post_types: 'linked',
-				include: posts,
-				map,
-		  }
-		: false;
+	const linkedQuery = {
+		imc_layers: layer,
+		_fields: 'id,imc_icons,imc_loc,type',
+		post_types: 'linked',
+		map,
+	};
 
-	const linked = useEntityRecords( 'postType', 'imc-marker', linkedQuery );
+	if (posts) linkedQuery.include = posts;
 
-	// eslint-disable-next-line no-nested-ternary
-	const linkedMarkerRecords = listIfIncluded( posts, linked.records );
+	const linked = useEntityRecords('postType', 'imc-marker', linkedQuery);
+
+	const linkedMarkerRecords = Array.isArray(linked.records)
+		? linked.records
+		: [];
 
 	// Get standalone markers.
 	const saQuery = showStandAlone
 		? {
-				layers: layer,
+				imc_layers: layer,
 				_fields: 'id,imc_icons,imc_loc,type',
 				post_types: 'standalone',
 				map,
 		  }
 		: false;
 
-	const standAlone = useEntityRecords( 'postType', 'imc-marker', saQuery );
+	const standAlone = useEntityRecords('postType', 'imc-marker', saQuery);
 
-	const saMarkerRecords = listIfIncluded(
-		showStandAlone,
-		standAlone.records
-	);
+	const saMarkerRecords = Array.isArray(standAlone.records)
+		? standAlone.records
+		: [];
 
 	// Notify user if there is an issue fetching the markers.
 	useNotice(
-		( posts && linked.status === 'ERROR' ) ||
-			( showStandAlone && standAlone?.status === 'ERROR' ),
+		linked.status === 'ERROR' ||
+			(showStandAlone && standAlone?.status === 'ERROR'),
 		__(
 			'Error loading correct markers. Please refresh the application to try again.',
 			'flare-imc'
 		),
-		[ posts, linked.status, showStandAlone, standAlone?.status ]
+		[posts, linked.status, showStandAlone, standAlone?.status]
 	);
 
 	// Merge post and standalone markers once both have been fetched.
-	if (
-		! Array.isArray( linkedMarkerRecords ) ||
-		! Array.isArray( saMarkerRecords )
-	)
-		return [];
+	if (linked.status === 'ERROR' || standAlone.status === 'ERROR') return [];
 
-	return [ ...linkedMarkerRecords, ...saMarkerRecords ];
+	return [...linkedMarkerRecords, ...saMarkerRecords];
 }
