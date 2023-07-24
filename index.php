84dfbe7bb3c2ccef3e11441d4e592ae163d9f6e6
igor <igor@flarehub.io>
2023-07-24 18:14:39 +0300

    Patch version update

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  * Author URI:          https://flarehub.io
  * Text Domain:         flare-imc
  * Domain Path:         /languages
- * Version:             0.1.4
+ * Version:             0.1.5
  *                      (Also update block.json when updating version number)
  * Requires at least:   5.0
  * License:             GPLv3
